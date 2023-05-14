dfe1a2b7092c1fa41ac5ff92a1067b72c71a4fa9
Seth van Buren <18116602+sethkor@users.noreply.github.com>
2023-05-15 07:45:16 +1000

    Remove .idea stuff

---
+++ crontab.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load the shell environment
+source ~/.zshrc
+PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+# Execute your desired command or script
+/Users/seth/dev/redback-pvoutput/redback-pvoutput.sh
